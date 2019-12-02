93f957159e611c65ae6dd1d4f7595d322cc82335
James Johnson <music1384@yahoo.co.uk>
2019-12-02 22:01:54 +0000

    Create helper regulators
    Create regulators to help control how often a task should update.

---
+++ AIAssignment/Assets/Scripts/AI System/Regulator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.AI_System
+{
+    class Regulator
+    {
+        /// <summary>The current time that has elapsed for this regulator.</summary>
+        private float _Timer = 0.0f;
+        /// <summary>The delay between task calls.</summary>
+        private float _ProcessDelay = 0.0f;
+
+        /// <summary>Creates a new regulator which will only allow a task to fire every few seconds.</summary>
+        /// <param name="delay">The delay (in seconds) before the task can happen.</param>
+        public Regulator (float delay)
+        {
+            _ProcessDelay = delay;
+        }
+
+        /// <summary>Determines if a task can process.</summary>
+        /// <returns>True if the task is allowed to process.</returns>
+        public bool CanProcess ()
+        {
+            _Timer += UnityEngine.Time.deltaTime;
+
+            if (_Timer >= _ProcessDelay)
+            {
+                _Timer = 0.0f;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
