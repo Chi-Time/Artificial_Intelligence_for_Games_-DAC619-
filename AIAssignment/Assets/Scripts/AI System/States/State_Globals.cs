df3a405241ab2b8b4bdca176bfec3a1f6bcccda9
James Johnson <music1384@yahoo.co.uk>
2019-12-02 22:10:58 +0000

    Update State_Globals
    Update global state to have more efficient use of desired state

--- AIAssignment/Assets/Scripts/AI System/States/State_Globals.cs
+++ AIAssignment/Assets/Scripts/AI System/States/State_Globals.cs
@@ -18,11 +18,13 @@ namespace Assets.Scripts.AI_System.States
 
         public void Enter (AI agent)
         {
-
+            Log.EnteredState ("Globals", agent);
         }
 
         public void Process (AI agent)
         {
+            Log.ProcessingState ("Globals", agent);
+
             // Default values for both desirabiltiy and evaluator.
             float bestDesirability = 0.0f;
             IEvaluator<AI> bestEvaluator = null;
@@ -30,40 +32,39 @@ namespace Assets.Scripts.AI_System.States
             // Loop through every evaluator.
             foreach (IEvaluator<AI> evaluator in _Evaluators)
             {
-                //
+                //Calculate the desirability of the current task.
                 float desirability = evaluator.CalculateDesirability (agent);
 
+                // If the desirability of this given evaluator is higher than a previous.
                 if (desirability >= bestDesirability)
                 {
+                    // It then becomes our next best one.
                     bestDesirability = desirability;
                     bestEvaluator = evaluator;
                 }
             }
 
+            // Grab the desired state from our best evaluator.
+            var desiredState = bestEvaluator.GetState ();
+
             // Check to see if we are already in the desired state. If we are, then leave as there's nothing more to do.
-            if (agent.Brain.IsInState (bestEvaluator.GetState ()) == false)
+            if (agent.Brain.IsInState (desiredState) == false)
                 return;
             // If we're not, then switch to the newly desired state.
             else
-                agent.Brain.ChangeState (bestEvaluator.GetState ());
-
-            //Log.Desirability (Aspirations.Evaluator_AttackEnemy (agent), agent);
-
-            /** Get all evaluators
-             *  Find evalutor with highest desirabiliy
-             *  Find evaluator relevent state
-             *  Change state to evaluator relevent state.
-             */
+                agent.Brain.ChangeState (desiredState);
         }
 
         public void Exit (AI agent)
         {
-
+            Log.ExitedState ("Globals", agent);
         }
 
         public bool HandleMessage (Message message)
         {
             return false;
         }
+
+        public void AddSubState (IState<AI> subState) { throw new NotImplementedException (); }
     }
 }
