e0f5b7b986bccf8467a90d84845caa6bab771f15
James Johnson <music1384@yahoo.co.uk>
2019-12-03 16:28:22 +0000

    Refactor and rename curves

---
+++ AIAssignment/Assets/Scripts/AI System/UtilityCurves.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.AI_System
+{
+    class UtilityCurves : MonoBehaviour
+    {
+        public static UtilityCurves Instance { get; private set; }
+
+        public static AnimationCurve Step { get; private set; }
+        public static AnimationCurve Linear { get; private set; }
+        public static AnimationCurve EaseOut { get; private set; }
+        public static AnimationCurve Logistic { get; private set; }
+        public static AnimationCurve Exponential { get; private set; }
+        public static AnimationCurve ReverseExponential { get; private set; }
+        
+        [SerializeField] private AnimationCurve _Step = null;
+        [SerializeField] private AnimationCurve _Linear = null;
+        [SerializeField] private AnimationCurve _EaseOut = null;
+        [SerializeField] private AnimationCurve _Logistic = null;
+        [SerializeField] private AnimationCurve _Exponential = null;
+        [SerializeField] private AnimationCurve _ReverseExponential = null;
+        
+
+        private void Awake ()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy (this);
+
+            SetStaticFields ();
+        }
+
+        private void SetStaticFields ()
+        {
+            // Set the static fields with the curves made in the inspector.
+            Step = _Step;
+            Linear = _Linear;
+            EaseOut = _EaseOut;
+            Logistic = _Logistic;
+            Exponential = _Exponential;
+            ReverseExponential = _ReverseExponential;
+
+            // Set the fields back to null so that we're not holding onto the inspector vars anymore.
+            _Step = null;
+            _Linear = null;
+            _EaseOut = null;
+            _Logistic = null;
+            _Exponential = null;
+            _ReverseExponential = null;
+        }
+
+        [ContextMenu ("Test Linear")]
+        public void TestLinear ()
+        {
+        }
+    }
+}
