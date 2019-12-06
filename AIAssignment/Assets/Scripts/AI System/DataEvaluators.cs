6128b998fb4a7b0f452970aa44ae52aded5626a2
James Johnson <music1384@yahoo.co.uk>
2019-12-06 11:42:14 +0000

    Cleaned data evaluators

--- AIAssignment/Assets/Scripts/AI System/DataEvaluators.cs
+++ AIAssignment/Assets/Scripts/AI System/DataEvaluators.cs
@@ -7,69 +7,50 @@ using UnityEngine;
 
 namespace Assets.Scripts.AI_System
 {
+    /// <summary>Used for accessing game data in a clean value range.</summary>
     class DataEvaluators
     {
-        /// <summary>Evaluates the desirability of the distance to an object between a range of 0-1</summary>
-        /// <param name="agent">The current agent wanting the object.</param>
+        /// <summary>Evaluates the desirability of the distance to an object between a range of 0-1. 0 being most desirable and 1 being least.</summary>
+        /// <param name="agent">The agent wanting the distance.</param>
         /// <param name="obj">The object in question.</param>
         /// <returns>A normalised value between a range of 0-1 representing the desirability.</returns>
         public static float DistanceToObject (GameObject agent, GameObject obj)
         {
+            // If either target is null then return 1 as this isn't a desirable option.
             if (obj == null || agent == null)
                 return 1.0f;
 
-            const float minDistance = 0f;
-            const float maxDistance = 75f;
-            float distance = Vector3.Distance (agent.transform.position, obj.transform.position);
-
-            if (distance > maxDistance)
-                return 1.0f;
-
-            float desirability = Helpers.GetDistribution (distance, minDistance, maxDistance);
-
-            //Debug.Log ("Distance Utility: " + UtilityCurves.Linear.Evaluate (desirability));
-
-            return UtilityCurves.Linear.Evaluate (desirability);
+            return DistanceToPosition (agent, obj.transform.position);
         }
 
-        /// <summary>Evaluates the desirability of the distance to an object between a range of 0-1</summary>
-        /// <param name="agent">The current agent wanting the object.</param>
+        /// <summary>Evaluates the desirability of the distance to an object between a range of 0-1. 0 being most desirable and 1 being least.</summary>
+        /// <param name="agent">The agent wanting the distance.</param>
         /// <param name="position">The position in question.</param>
         /// <returns>A normalised value between a range of 0-1 representing the desirability.</returns>
         public static float DistanceToPosition (GameObject agent, Vector3 position)
         {
-            if (position == Vector3.zero)
-                return 0.0f;
-
+            // Find our distance and set up a basic range.
             const float minDistance = 0f;
             const float maxDistance = 75f;
             float distance = Vector3.Distance (agent.transform.position, position);
 
+            // If we're further away than max distance than this option is undesirable.
             if (distance > maxDistance)
                 return 1.0f;
 
+            // Clamp and normalise the value and return it.
             float desirability = Helpers.GetDistribution (distance, minDistance, maxDistance);
 
-            //Debug.Log ("Distance Utility: " + UtilityCurves.Linear.Evaluate (desirability));
-
-            return UtilityCurves.Linear.Evaluate (desirability);
+            return desirability;
         }
 
-        public static float TeamScore (AI agent)
-        {
-            if (agent.Data.FriendlyScore < agent.Data.EnemyScore)
-                return Helpers.GetDistribution (agent.Data.FriendlyScore, 0.0f, agent.Data.EnemyScore);
-            else
-                return 0.01f;
-        }
-
+        /// <summary>Get the current agent's health as a normalised value between 0-1. 0 Being least desirable and 1 being most.</summary>
+        /// <param name="agent">The agent target.</param>
+        /// <returns>A normalised value clamped between a 0-1 range.</returns>
         public static float Health (AI agent)
         {
             float desirability = Helpers.GetDistribution (agent.Data.CurrentHitPoints, 0.0f, agent.Data.MaxHitPoints);
 
-            //Debug.Log ("Health Utility: " + UtilityCurves.Logistic.Evaluate (desirability));
-
-            // Use a sigmoid logistic curve to get the overall desirability for health.
             return desirability;
         }
 
@@ -78,57 +59,25 @@ namespace Assets.Scripts.AI_System
         /// <returns>The clamped and normalised value between 0 - 1</returns>
         public static float NumberOfEnemiesInSight (AI agent)
         {
+            // Constant for dividing the value in half so it doesn't weigh too heavily in equations.
             const float half = 0.5f;
             float desirability = 0.0f;
 
             int enemiesInView = agent.Senses.GetEnemiesInView ().Count;
-
             desirability = Helpers.GetDistribution (enemiesInView, 0, 3);
 
             return desirability * half;
         }
 
-        /// <summary>Evaluates the current number of friendlies into a 0-1 range with 1 being desirable and 0 being undesirable.</summary>
-        /// <param name="agent">The agent to use for determining friendlies around them.</param>
-        /// <returns>The clamped and normalised value between 0 - 1</returns>
-        public static float NumberOfFriendliesInSight (AI agent)
-        {
-            const float half = 0.5f;
-            float desirability = 0.0f;
-
-            int friendliesInView = agent.Senses.GetFriendliesInView ().Count;
-
-            desirability = Helpers.GetDistribution (friendliesInView, 0, 2);
-
-            return desirability * half;
-        }
-
-        public static float NumberOfFriendliesAtPosition (AI agent, Vector3 position)
-        {
-            float desirability = 0.0f;
-            const float minDistance = 7.5f;
-
-            var friendlies = WorldManager.Instance.GetTeammates (agent);
-            int friendliesAtPosition = 0;
-
-            foreach (AI member in friendlies)
-            {
-                if (member == agent)
-                    continue;
-
-                if (Helpers.IsNearPosition (member.transform.position, position, minDistance))
-                    friendliesAtPosition++;
-            }
-
-            desirability = Helpers.GetDistribution (friendliesAtPosition, 0, 2);
-
-            return desirability;
-        }
-
+        /// <summary>Get's the current agent's strength as a normalised valued between 0-1. 0 being least desirable and 1 being most.</summary>
+        /// <param name="agent">The agent target.</param>
+        /// <returns>A normalised and clamped value between a 0-1 range.</returns>
         public static float Strength (AI agent)
         {
             float desirability = 0.0f;
+            // Pre-defined tweaker value for adjusting strength.
             const float tweaker = 1.5f;
+            // Get the agent's current attack damage.
             float damage = agent.Data.NormalAttackDamage;
 
             // If the agent has a powerup.
@@ -142,10 +91,10 @@ namespace Assets.Scripts.AI_System
                 return desirability;
             }
 
+            // Apply the tweaker to their damage and then get a normalised value from it and return.
             damage *= tweaker;
             desirability = Helpers.GetDistribution (damage, 0, 20);
 
-            //Debug.Log ("Strength Utility: " + desirability);
             return desirability;
         }
     }
